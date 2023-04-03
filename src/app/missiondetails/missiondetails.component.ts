b4c1298d165141056a0b200924e15147c1cb2fd4
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-04-03 18:55:03 -0400

    done

--- src/app/missiondetails/missiondetails.component.ts
+++ src/app/missiondetails/missiondetails.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Mission } from 'src/app/models/mission';
+import { SpacexapiService } from '../network/spacexapi.service';
 
 @Component({
   selector: 'app-missiondetails',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./missiondetails.component.css']
 })
 export class MissiondetailsComponent {
+  flight_number: number;
+  missionDetails!: Mission;
+
+  constructor(
+    public dialogueRef: MatDialogRef<MissiondetailsComponent>,
+    @Inject(MAT_DIALOG_DATA) public mission: any,
+    private spacexApi: SpacexapiService,
+  ) {
+    this.flight_number = mission.flight_number;
+  }
+
+  ngOnInit() {
+    this.spacexApi.getMission(this.flight_number)
+      .subscribe(
+        (mission: any) => {
+          this.missionDetails = mission;
+          console.log(mission)
+        }
+      );
+        console.log(this.missionDetails)
+  }
+
 
 }
