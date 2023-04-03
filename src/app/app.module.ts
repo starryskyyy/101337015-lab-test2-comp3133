d55d2305796aa2d3de3aacf0a2544b8d47d92114
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-04-03 16:42:36 -0400

    added mission details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MissiondetailsComponent } from './missiondetails/missiondetails.component';
+import { MissionlistComponent } from './missionlist/missionlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MissiondetailsComponent,
+    MissionlistComponent
   ],
   imports: [
     BrowserModule,
