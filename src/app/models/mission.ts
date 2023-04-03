ebabb1a0decf582c2527db1014d9bc2fee170c44
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-04-03 18:54:40 -0400

    Update mission.ts

--- src/app/models/mission.ts
+++ src/app/models/mission.ts
@@ -3,15 +3,24 @@ export class Mission{
     launch_year: number
     details: string
     flight_number: number
+    rocket: {
+        rocket_name: string
+        rocket_type: string
+    }
+    launch_site: {
+        site_name_long: string
+    }
     links: {
         mission_patch_small: string;
       }
 
-    constructor(mission_name: string, launch_year: number, details: string, links: { mission_patch_small: string }, flight_number: number){
+    constructor(mission_name: string, launch_year: number, details: string, links: { mission_patch_small: string }, flight_number: number, rocket: { rocket_name: string, rocket_type: string }, launch_site: { site_name_long: string }){
         this.mission_name = mission_name
         this.launch_year = launch_year
         this.details = details
         this.links = links
         this.flight_number = flight_number
+        this.rocket = rocket
+        this.launch_site = launch_site
     }
 }
