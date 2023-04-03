76b1b19c73e58bc2866d517a98fbf455043f3a01
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-04-03 17:05:18 -0400

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MissiondetailsComponent } from './missiondetails/missiondetails.component';
 import { MissionlistComponent } from './missionlist/missionlist.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'; 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +22,16 @@ import { MissionlistComponent } from './missionlist/missionlist.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatIconModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
