45873387b7da5057aec7428c7664e545cebc9292
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-04-03 16:42:24 -0400

    added mission list

---
+++ src/app/missionlist/missionlist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-missionlist',
+  templateUrl: './missionlist.component.html',
+  styleUrls: ['./missionlist.component.css']
+})
+export class MissionlistComponent {
+
+}
